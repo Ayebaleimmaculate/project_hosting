1d0c02039df73decdf883edb34bc859712039bec
rhodinemma <rhodinemma10@gmail.com>
2024-09-27 20:32:39 +0300

    chore: update dockerfile

--- backend/config.py
+++ backend/config.py
@@ -1,6 +1,10 @@
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:@localhost/bakeryapi'
+    # SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://mvnuaofrvxbmjkai:immaculate@102.134.147.233:32764/bakeryapi'
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://mvnuaofrvxbmjkai:immaculate@102.134.147.233:32764/ywobruzspddayordfttvcomn'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     JWT_SECRET_KEY = 'bakery'
